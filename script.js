fb79c5a2bede23c5373755323cb3d51126561846
abhi20-1996 <110686880+abhi20-1996@users.noreply.github.com>
2023-05-18 22:08:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,5 @@ const table= document.getElementById("sampleTable");
 const row=table.insertRow(0);
 const cell1=row.insertCell(0);
 const cell2=row.insertCell(1);
-
-	
-  
-}
+cell1.innerHTML="New Cell1";
+cell2.innerHTML="New Cell2";
